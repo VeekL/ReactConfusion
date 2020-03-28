16a437875bcf3bec662e53380ae37b1a870bea51
VeekL <veeklee92@gmail.com>
2020-03-28 21:14:46 +0800

    React Redux Forms Revisited

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -4,6 +4,7 @@ import { Breadcrumb, BreadcrumbItem, Nav, NavItem,
     Row, Label, Col } from 'reactstrap';
 import {Control, Errors, LocalForm} from 'react-redux-form';        
 import { Link} from 'react-router-dom';
+import { Loading } from './LoadingComponent';
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
@@ -153,8 +154,25 @@ class CommentForm extends Component{
 }
 
     const DishDetail = (props) => {
-            console.log('Dishdetail Component render invoked');
-        if(props.dish){
+        if (props.isLoading) {
+            return(
+                <div className="container">
+                    <div className="row">            
+                        <Loading />
+                    </div>
+                </div>
+            );
+        }
+        else if (props.errMess) {
+            return(
+                <div className="container">
+                    <div className="row">            
+                        <h4>{props.errMess}</h4>
+                    </div>
+                </div>
+            );
+        }
+        else if (props.dish != null) 
             return (
                 <div className="container">
                     <div className="row">
@@ -175,7 +193,6 @@ class CommentForm extends Component{
                     </div>
                 </div>
             );
-        }
         else{
             return(
                 <div>
