d10ac12742db959669be141130fdfa8e28e29b4f
VeekL <veeklee92@gmail.com>
2020-03-30 19:36:55 +0800

    React Animations, React Form

--- src/redux/forms.js
+++ src/redux/forms.js
@@ -1,3 +1,5 @@
+import * as ActionTypes from './ActionTypes';
+
 export const InitialFeedback = {
     firstname: '',
     lastname: '',
@@ -6,4 +8,25 @@ export const InitialFeedback = {
     agree: false,
     contactType: 'Tel.',
     message: ''
+};
+
+
+
+export const Feedback = (state = { errMess: null, feedback:[]}, action) => {
+  switch (action.type) {
+    case ActionTypes.ADD_FEEDBACK:
+      return {...state, errMess: null, feedback: action.payload};
+
+    case ActionTypes.FEEDBACK_FAILED:
+      return {...state, errMess: action.payload};
+
+    case ActionTypes.ADD_COMMENT:
+        var feedback = action.payload;
+        feedback.id = state.feedback.length;
+        feedback.date = new Date().toISOString();
+        return { ...state, feedback: state.feedback.concat(feedback)};
+
+    default:
+      return state;
+  }
 };
